526bd6af85e45834c107e47e706d6ae8f532fa67
Jeremy Novak <jgnovak@gmail.com>
2016-03-16 15:26:00 -0500

    Project organization
    Project organization

---
+++ Project/Chapter 4/Finish/TimberJim/TimberJim/Classes/Player.h
@@ -0,0 +1,13 @@
+//
+//  Player.h
+//  TimberJim
+//
+//  Created by Jeremy Novak on 3/16/16.
+//  Copyright © 2016 Jeremy Novak. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface Player : SKSpriteNode
+
+@end
